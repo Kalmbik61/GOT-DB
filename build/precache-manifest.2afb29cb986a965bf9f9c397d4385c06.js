c94059601ef3a3e4d96ee8b19409a8c70e9a9e6b
Kalmbik61 <59443234+Kalmbik61@users.noreply.github.com>
2020-10-19 14:35:54 +0300

    Add files via upload

---
+++ build/precache-manifest.2afb29cb986a965bf9f9c397d4385c06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e37c8b65cbf7ef62082eaa83a5f0eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e195cff9490112545c83",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a1b404129c5c09b821c0",
+    "url": "/static/css/main.8d2f6a11.chunk.css"
+  },
+  {
+    "revision": "e195cff9490112545c83",
+    "url": "/static/js/2.6b35b247.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6b35b247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b404129c5c09b821c0",
+    "url": "/static/js/main.87e7e4ce.chunk.js"
+  },
+  {
+    "revision": "358d73b19b5362f868a9",
+    "url": "/static/js/runtime-main.b60b1a8c.js"
+  },
+  {
+    "revision": "a3027ede1df2d091f30c7689eb04fc44",
+    "url": "/static/media/error.a3027ede.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/static/media/got.4da81083.jpeg"
+  }
+]);
